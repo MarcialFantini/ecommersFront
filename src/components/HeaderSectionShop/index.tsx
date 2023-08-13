e13fb0f9641b9330c75d826cbfc4154111bd8b0e
MarcialFantini <marcialfantini@gmail.com>
2023-08-13 08:33:23 -0300

    header shop ready

--- src/components/HeaderSectionShop/index.tsx
+++ src/components/HeaderSectionShop/index.tsx
@@ -2,31 +2,43 @@ import React from "react";
 import style from "./style.module.css";
 import Image from "next/image";
 
+import prueba from "../../assets/objectoPrueba/person.jpg";
+
 function HeaderSectionShop() {
   return (
-    <div className={style.containerHeader}>
-      <div className={style.containerCardGreat}>
-        <h2>producto</h2>
-        <picture>
-          <Image src={""} alt=""></Image>
+    <header className={style.headerContainer}>
+      <div className={style.containerLeft}>
+        <picture className={style.picture}>
+          <Image
+            className={style.img + " " + style.imgPrimary}
+            src={prueba}
+            alt="Logo de Real Shop Network"
+          />
         </picture>
       </div>
-      <div>
-        <div>
-          <h2>producto</h2>
-          <picture>
-            <Image src={""} alt=""></Image>
-          </picture>
+
+      <div className={style.containerRight}>
+        <div className={style.topContainerSub}>
+          <h1>Welcome to Real Shop Network</h1>
+          <p>Discover the best shopping experience.</p>
         </div>
 
-        <div>
-          <h2>producto</h2>
-          <picture>
-            <Image src={""} alt=""></Image>
-          </picture>
+        <div className={style.containerRightBottom}>
+          <div className={style.containerImgSub}>
+            <picture className={style.picture}>
+              <Image className={style.img} src={prueba} alt="Trending Item 1" />
+            </picture>
+            <h2>#trending</h2>
+          </div>
+          <div className={style.containerImgSub}>
+            <picture className={style.picture}>
+              <Image className={style.img} src={prueba} alt="Trending Item 2" />
+            </picture>
+            <h2>#trending</h2>
+          </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
 
