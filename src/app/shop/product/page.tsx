c9982ec049592c251762b153a09541c613afbedc
MarcialFantini <marcialfantini@gmail.com>
2023-08-13 09:41:45 -0300

    producto individual maquetado

--- src/app/shop/product/page.tsx
+++ src/app/shop/product/page.tsx
@@ -3,24 +3,26 @@ import React from "react";
 
 import style from "./style.module.css";
 
-import girl from "../../../assets/objectoPrueba/girlVertical.jpg";
-import objeto from "../../../assets/objectoPrueba/objeto.png";
+import objeto from "../../../assets/objectoPrueba/person.jpg";
 
 export default function ProductRouter() {
   return (
     <div className={style.containerProduct}>
-      <picture className={style.pictureLeft}>
-        <Image className={style.imgLeft} src={girl} alt="a"></Image>
+      <picture className={style.picture}>
+        <Image className={style.img} src={objeto} alt="a"></Image>
       </picture>
 
       <div className={style.containerProductRight}>
         <h1 className={style.title}>Titulo product</h1>
         <p className={style.subTitle}>NMD CITY SOCK 2</p>
-        <picture className={style.pictureRight}>
-          <Image className={style.imgRight} src={objeto} alt="" />
-        </picture>
-
-        <button className={style.button}>Comprar</button>
+        <p className={style.description}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          facere ratione et officia. Sint quasi perferendis atque omnis aliquid
+          deserunt voluptatibus quia quaerat, ipsam quam fugiat, blanditiis,
+          aperiam illum neque?
+        </p>
+        <p className={style.price}>$23</p>
+        <button className={style.button}>BUY NOW</button>
       </div>
     </div>
   );
